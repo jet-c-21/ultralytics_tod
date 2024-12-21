075cfd0b3081dae256a6f557ad2f08e40133e3fd
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-12-21 13:37:03 +0800

    Update .gitignore to exclude train-datasets and refactor download_training_datasets.py to remove ultralytics dependency

--- py_scripts/nas/download_training_datasets.py
+++ py_scripts/nas/download_training_datasets.py
@@ -15,15 +15,10 @@ sys.path.append(str(PROJECT_DIR))
 print(f"[*INFO*] - append directory to path: {PROJECT_DIR}")
 # <<< Dynamic Changing `sys.path` in Runtime by Adding Project Directory to Path <<<
 
-import ultralytics
-print(f"[*INFO*] - imported ultralytics path: {ultralytics.__file__}")
-print(f"[*INFO*] - imported ultralytics version: {ultralytics.__version__}")
-
-from ultralytics import YOLO
+from thirdparty_packages.ichase_utils import var as VAR
 
 def main():
-    model = YOLO("yolov8n.pt")
-    model.info()
+    pass
 
 if __name__ == '__main__':
     main()
